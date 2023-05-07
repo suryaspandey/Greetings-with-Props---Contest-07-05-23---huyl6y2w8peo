49110e35d245902f219a0b44d969a56a2f8799d2
Surya Pandey <suryaspandey@gmail.com>
2023-05-07 10:30:48 +0530

    Update Welcome.js

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,1 +1,12 @@
 // write code for Welcome component here
+
+export default Welcome;
+
+function Welcome({ name }) {
+    return (
+        <>
+            <h1>Hey {name}!</h1>
+            <h2>Welcome to Newton School</h2>
+        </>
+    );
+}
