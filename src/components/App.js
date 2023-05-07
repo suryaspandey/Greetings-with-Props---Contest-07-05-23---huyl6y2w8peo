7a9a45ebf82fdfab59d3e737602f35d0ffb7d3b4
Surya Pandey <suryaspandey@gmail.com>
2023-05-07 10:29:29 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -3,8 +3,9 @@ import '../styles/App.css';
 import Welcome from './Welcome';
 
 const App = () => {
+  const name = "Surya";
   return (
-    <Welcome name="" />
+    <Welcome name="{name}" />
   )
 }
 
